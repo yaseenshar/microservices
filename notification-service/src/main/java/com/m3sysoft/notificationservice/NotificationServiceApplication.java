4b31183cab445dd53b3e78dbcedab4838921ab92
yaseen <yaseenshar50@gmail.com>
2024-01-31 00:15:36 +0100

    Microservices with ready to release demo.

--- notification-service/src/main/java/com/m3sysoft/notificationservice/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/m3sysoft/notificationservice/NotificationServiceApplication.java
@@ -1,3 +1,6 @@
+package com.m3sysoft.notificationservice;
+
+import com.m3sysoft.notificationservice.listener.OrderPlacedEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
